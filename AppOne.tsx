e31f55a4b89dbcc7864f2d7e40c4251b8c97b638
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-07-19 23:06:18 +0530

    Fixed all prettier errors and fixed few style bugs

--- AppOne.tsx
+++ AppOne.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
 
 function AppOne(): JSX.Element {
     const isDarkMode = useColorScheme() === 'light';
     return (
-        <View style={styles}>
+        <View style={styles.container}>
             <Text style={isDarkMode ? styles.whiteText : styles.darkText}>Bismillah</Text>
         </View>
     );
@@ -15,19 +15,19 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     whiteText: {
         fontSize: 20,
         fontWeight: 'bold',
-        backgroundColor: 'skyblue'
+        backgroundColor: 'skyblue',
     },
     darkText: {
         fontSize: 50,
         fontWeight: 'bold',
-        backgroundColor:'darkblue',
-        color: 'green'
+        backgroundColor: 'darkblue',
+        color: 'green',
     },
-})
+});
 
 export default AppOne;
