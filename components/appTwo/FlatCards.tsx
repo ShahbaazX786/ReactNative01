b7111cb0b3595dfe9638d33e02ab5d8f51b61260
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-07-19 23:06:34 +0530

    fixed prettier formatting

---
+++ components/appTwo/FlatCards.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+export default function FlatCards(): JSX.Element {
+    return (
+        <View>
+            <Text>FlatCards</Text>
+        </View>
+    );
+}
+
+// const styles = StyleSheet.create({});
