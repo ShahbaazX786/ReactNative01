df93b5c7612080c6f20f31660b95faf93cad2a4f
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-07-20 15:11:08 +0530

    Added a new component

--- AppTwo.tsx
+++ AppTwo.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { SafeAreaView, ScrollView } from 'react-native';
 import FlatCards from './components/appTwo/FlatCards';
+import ElevatedCard from './components/appTwo/ElevatedCard';
 
 export default function AppTwo(): JSX.Element {
     return (
         <SafeAreaView>
             <ScrollView>
                 <FlatCards />
+                <ElevatedCard />
             </ScrollView>
         </SafeAreaView>
     );
