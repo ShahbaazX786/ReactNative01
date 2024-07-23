810cbc3c9161bf4e2bc6771f616add792e0d1bca
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-07-23 19:59:35 +0530

    Updated main view component

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import { AppRegistry } from 'react-native';
 // import App from './App';
 // import AppOne from './AppOne';
 // import AppTwo from './AppTwo';
-import AppThree from './AppThree';
+// import AppThree from './AppThree';
+import AppFour from './AppFour';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => AppThree);
+AppRegistry.registerComponent(appName, () => AppFour);
